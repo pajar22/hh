10a74022ced0cd4ee502a4361055c4fc88b38e5e
pajar22 <44915350+pajar22@users.noreply.github.com>
2018-11-10 04:02:32 +0000

    Update bukutamu.php

--- bukutamu.php
+++ bukutamu.php
@@ -1,31 +1,14 @@
 <html>
 <head>
-<title>Buku Tamu</title>
-</head>
 <body>
-<h1>PEMILIHAN KETUA OSIS</h1>
-Masa Jabatan 2018-2019
-<hr>
+<h1>Daftar barang </h1>
 <FORM ACTION="simpan.php" METHOD="GET">
 Nama:
-  <INPUT TYPE="TEXT" NAME="nama"><p>
-Kelas:
-  <INPUT TYPE="TEXT" NAME="kls"><p>
-
-
-Pilihan Ketua Osis  : <br>
-<table width="417" border="1">
-<tr>
-<td width="202" height="100"><img src="Koala.jpg" width="202" height="159"/></td>
-<td width="199"><img src="Jellyfish.jpg"width="199" height="158"/></td>
-</tr>
-<tr>
-<td height="37"><p align="center"><INPUT TYPE="RADIO" VALUE="Lutfi Dengan Komar" CHECKED NAME="pilihan">Lutfi Dengan Komarudin<br></p></td>
-<td><p align="center"><INPUT TYPE="RADIO" VALUE="Tika Dengan Ali" NAME="pilihan">Tika Dengan Ali</p></td>
-  </tr>
-  </table>
-  <p>
-
+<INPUT TYPE="TEXT" NAME="nama"><br>
+Barang:
+<INPUT TYPE="TEXT" NAME="brg"><br>
+Jumlah:
+<INPUT TYPE="TEXT" NAME="jml"><br>
 <INPUT TYPE="SUBMIT" VALUE="simpan">
 </FORM>
 </body>
