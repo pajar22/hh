0f96f475816d5fb9cb0463f4a9019922f91c4389
pajar22 <44915350+pajar22@users.noreply.github.com>
2018-11-10 04:03:50 +0000

    Update simpan.php

--- simpan.php
+++ simpan.php
@@ -1,21 +1,21 @@
 <html>
-<head><title>Buku Tamu</title>
+<head><title>Pesanan</title>
 </head>
 
 <body>
 <?php
 //peroleh variabel URL
 $nama=$_GET["nama"];
-$pilihan=$_GET["pilihan"];
+$Barang=$_GET["brg"];
+$Jumlah=$_GET["jml"];
 
-//simpan
 $pegangan=fopen("BUKUTAMU.DAT","a");
 fputs($pegangan,$nama."\n");
-fputs($pegangan,$pilihan."\n");
+fputs($pegangan,$brg."\n");
+fputs($pegangan,$jml."\n");
 fclose($pegangan);
 
-echo("Halo, $nama. TerimaKasih Telah Berpartisipasi....<BR>\n");
-
+echo("Halo, $nama. TerimaKasih Pesanan Barang Sudah Diterima....<BR>\n");
 ?>
 
 </body>
